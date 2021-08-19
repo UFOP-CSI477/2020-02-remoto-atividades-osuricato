252c33778d7be41a01bfed96303b8657eff217d7
osuricato <samuel.souzagomes001@gmail.com>
2021-08-19 16:26:26 -0300

    prepare environment frontend

--- Projeto/web/src/App.tsx
+++ Projeto/web/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import Global from './styles/global';
 
-function App() {
+import { AuthProvider } from './hooks/auth';
+import { SignMenuProvider } from './hooks/toggle';
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <SignMenuProvider>
+          <Routes />
+        </SignMenuProvider>
+      </AuthProvider>
+      <Global />
     </div>
   );
-}
-
-export default App;
+};
